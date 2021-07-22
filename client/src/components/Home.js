3ee39a28089dfc340b3951b57076239379a58bb6
Ian French <ianfrench33@gmail.com>
2021-07-22 16:57:51 -0400

    worked on front end main room page. currently adding messages to page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,12 @@
-import { useState, useEffect } from "react"
-const Home = (() => {
-    const [message, setMessage] = useState('')
-
-    useEffect(() => {
-        if(!message) {
-            fetch('/homeChatroom')
-                .then((res) => res.json())
-                .then((messageList) => {
-                    setMessage(messageList)
-                })
-        }
-    })
-
-    return (
-        <div>
-            <h1>home page</h1>
-            <form action="/homeMessage" method="POST">
-                <input type="text" name="author" placeholder="Username" />
-                <input type="text" name="messageBody" placeholder="What would you like to say?" />
-                <input type="submit" value="send" />
-            </form>
-        </div>
-    )
-})
+const Home = () => {
+  return (
+    <div>
+      <form action="/login" method="POST">
+        <input type="text" name="user" placeholder="Enter your username" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Home
