003a55cfc1dee97c8ae9dd4e3eeb1eb050ac2703
Ian French <ianfrench33@gmail.com>
2021-07-23 12:00:02 -0400

    added styles to room page. added header

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,37 @@
 import "../styles/home.css";
 
 const Home = () => {
-
   const submitName = () => {
-    let userName = document.getElementById("user-name").value
+    let userName = document.getElementById("user-name").value;
     //Store the username in a cookie
-    document.cookie = `userName=${userName}`
-    //Redirect the user to the main room 
-    document.location = "/room/main"
-  }
+    document.cookie = `userName=${userName}`;
+    //Redirect the user to the main room
+    document.location = "/room/main";
+  };
 
   return (
-    <div className="user-name-wrapper">
-      <div className="user-name-card">
-        <h1 className="user-title">Create a username:</h1>
-        <input className="user-name-input" id="user-name" type="text" name="user" placeholder="Enter your username" />
-        <input className="user-name-submit" type="submit" value="Submit" onClick={submitName} />
+    <div>
+      <h1 className="home-header">Chat App</h1>
+      <div className="user-name-wrapper">
+        <div className="user-name-card">
+          <h1 className="user-title">Create a username:</h1>
+          <input
+            className="user-name-input"
+            id="user-name"
+            type="text"
+            name="user"
+            placeholder="Enter your username"
+          />
+          <input
+            className="user-name-submit"
+            type="submit"
+            value="Submit"
+            onClick={submitName}
+          />
+        </div>
       </div>
-
-
     </div>
   );
 };
 
-export default Home
+export default Home;
--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import "../styles/room.css";
+import React from "react";
 
 const Room = () => {
   const [messages, setMessages] = useState("");
   const [user, setUser] = useState("");
 
   //Because slice returns an array, we need to take the first element
-  const roomID = window.location.href.split("/").slice(-1)[0]
+  const roomID = window.location.href.split("/").slice(-1)[0];
 
   async function submitMessage(evt) {
-    console.log(roomID)
+    console.log(roomID);
     //Make a post request to the "messages" endpoint.
     await fetch(`http://localhost:8000/rooms/${roomID}/messages`, {
       method: "POST",
@@ -17,15 +18,15 @@ const Room = () => {
       //JSON.stringify converts the body object into a string(Body object contains the user data we want to send to the server)
       body: JSON.stringify({
         author: user,
-        messageBody: document.getElementById("messageContent").value
+        messageBody: document.getElementById("messageContent").value,
       }),
       //content type header tells the express server the format that the data is in
       headers: {
-        "content-type": "application/json"
-      }
-    })
+        "content-type": "application/json",
+      },
+    });
     //Load the messages after a new message has been sent
-    getMessages()
+    getMessages();
   }
 
   function getMessages() {
@@ -33,51 +34,73 @@ const Room = () => {
     fetch(`http://localhost:8000/rooms/${roomID}/messages`)
       .then((res) => res.json())
       .then((res) => {
-        setMessages(res)
-      })
+        setMessages(res);
+      });
   }
 
   useEffect(() => {
     //Get the username from the cookie and set the user state variable. Split the cookie string by "=" since it holds key value pairs. Grab the second element from the array.
     setUser(document.cookie.split("=")[1]);
     //Run the get messages function in order to load in all the messages
-    getMessages()
+    getMessages();
 
     //Reload all messages every 10 seconds
     setInterval(() => {
-      getMessages()
-    }, 10000)
-
-
+      getMessages();
+    }, 10000);
   }, []);
 
   return (
     <div>
-      <h1 className="greeting">Hello {user}</h1>
+      <h1 className="home-header">Chat App</h1>
+
       <div className="container">
+        
         <div className="messages-wrapper">
-          <div className="messages">
-          <h3 className="chat-title">{roomID.charAt(0).toUpperCase()}{roomID.substring(1)} Chat</h3>
-            {messages ? (
-              //Messages refers to an array of message objects. Message refers to the individual message object itself(which holds author, messageBody, roomID etc.)
-              messages.map((message, index) => (
-                <div key={index}>
-                  <p><strong>{message.author}</strong>: {message.messageBody}</p>
-                  <p>Created on: {new Date(message.when).toLocaleString()}</p>
-                </div>
-              ))
-            ) : (
-              <p></p>
-            )}
-          </div>
+          <h1 className="greeting">Hello {user}</h1>
+          <div className="messages-rooms-wrapper">
+            <div className="messages">
+              <h3 className="chat-title">
+                {roomID.charAt(0).toUpperCase()}
+                {roomID.substring(1)} Chat
+              </h3>
+              {messages ? (
+                //Messages refers to an array of message objects. Message refers to the individual message object itself(which holds author, messageBody, roomID etc.)
+                messages.map((message, index) => (
+                  <div key={index}>
+                    <p>
+                      <strong>{message.author}</strong>: {message.messageBody}
+                    </p>
+                    <p>Created on: {new Date(message.when).toLocaleString()}</p>
+                  </div>
+                ))
+              ) : (
+                <p></p>
+              )}
+            </div>
 
-          <div className="rooms">
-            <h3 className="room-title">Rooms</h3>
-            <a href="/room/main">Main</a>
-            <a href="/room/sports">Sports</a>
-            <a href="/room/programming">Programming</a>
+            <div className="rooms">
+              <h3 className="room-title">Rooms</h3>
+              <a
+                className={roomID === "main" ? "highlight" : ""}
+                href="/room/main"
+              >
+                Main
+              </a>
+              <a
+                className={roomID === "sports" ? "highlight" : ""}
+                href="/room/sports"
+              >
+                Sports
+              </a>
+              <a
+                className={roomID === "programming" ? "highlight" : ""}
+                href="/room/programming"
+              >
+                Programming
+              </a>
+            </div>
           </div>
-
         </div>
         <div className="controls">
           <input
@@ -86,13 +109,14 @@ const Room = () => {
             name="messageBody"
             placeholder="What would you like to say?"
           />
-          <button onClick={submitMessage} value="send" >Submit</button>
-          <button onClick={getMessages} value="refresh" >Refresh</button>
+          <button className="control-button" onClick={submitMessage} value="send">
+            Submit
+          </button>
+          <button className="control-button" onClick={getMessages} value="refresh">
+            Refresh
+          </button>
         </div>
-
-
       </div>
-
     </div>
   );
 };
