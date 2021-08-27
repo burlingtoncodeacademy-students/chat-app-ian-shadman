f2be9ac251b2f01aafc4e4993b5d2130bdadee63
manofshad97 <shadman559@hotmail.com>
2021-08-27 16:31:49 -0400

    changed fetch location

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -36,7 +36,7 @@ const Room = () => {
       return;
     }
     //Make a post request to the "messages" endpoint.
-    await fetch(`http://localhost:8000/rooms/${roomID}/messages`, {
+    await fetch(`https://limitless-headland-30461.herokuapp.com/rooms/${roomID}/messages`, {
       method: "POST",
       //Send the username and message content
       //JSON.stringify converts the body object into a string(Body object contains the user data we want to send to the server)
